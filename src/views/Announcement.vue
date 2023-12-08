8f0099f4eb210155f29cd50f84478d2c60de5997
uryngaliev <zhainar999@gmail.com>
2023-12-08 13:04:09 +0600

    add counter

--- src/views/Announcement.vue
+++ src/views/Announcement.vue
@@ -5,11 +5,13 @@
       <h1 class="text-4xl mb-8 py-8">Обьявление</h1>
       <div class="text-center bg-pink-700 text-white">
         <p class="text-2xl py-4">
-          Дорогие родители приглашаем вас на утренник посвященному
-          ко дню Независимости! Вас с нетерпением будет ждать ваши любимые дети
-          группы "Ласточка"!
+          Дорогие родители приглашаем вас на утренник посвященному ко дню
+          Независимости! Вас с нетерпением будет ждать ваши любимые дети группы
+          "Ласточка"!
         </p>
       </div>
+      <web-component-embed id="a5myLz" app_slug="countdown-timer"></web-component-embed>
+      <Counter />
     </div>
   </div>
   <Footer class="bg-pink-700"></Footer>
